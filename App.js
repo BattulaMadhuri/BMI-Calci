a24ab8af4169dde47d08a480482127f39d488df5
Madhuri Battula <137879232+BattulaMadhuri@users.noreply.github.com>
2023-07-31 13:46:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+import './App.css';
+//import EmojiSearch from './components/EmojiSearch';
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+//import NameList from './components/NameList';
+// //import Greet from './components/Greet';
+// //import Welcome from './components/Welcome';
+// import Message from './components/Message';
+
+// function App(){
+//   return(
+//     <div className="App">
+//       {/* <FunctionClick />
+//       <ClassClick /> */}
+//       {/* <NameList /> */}
+//       <EmojiSearch />
+//      </div>
+//   )
+// }
+// export default App;
+
+
+// function App() {
+//   return (
+//     <div className="App"> 
+//     <Message />
+    
+//     {/* <Message />
+//      <Greet name="Madhu" heroName="Natasha" /> 
+//      <Greet name="Tarun" heroName="Iron Man"/> 
+//      <Greet name="Mahi" heroName="Thor"/> 
+//      <Welcome name="Madhu" /> */}
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// import Calc from './components/Calc';
+
+
+// function App(){
+//   return(
+//     <Calc />
+//   );
+// }
+// export default App;
+
+
+import BmiCalci from './components/Bmi';
+function App() {
+    return (
+      <div className="App">
+     <BmiCalci/>
+   </div>
+    );
+  }
+  
+  export default App;
